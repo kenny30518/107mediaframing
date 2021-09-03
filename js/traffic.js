93dc3504a3fc351b5521fa1c3b4eee72dd5ff9fc
kenny30518 <A107070036@mail.shu.edu.tw>
2021-09-03 15:14:30 +0800

    add new layoyt

--- js/traffic.js
+++ js/traffic.js
@@ -49,3 +49,20 @@ tl.reverse();
 $(document).on("click", ".rwdBurger", function() {
         tl.reversed(!tl.reversed());
 });
+
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget)
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
+    })
+    tabs.forEach(tab => {
+      tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+    target.classList.add('active')
+  })
+})

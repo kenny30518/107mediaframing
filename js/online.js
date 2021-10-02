d9ef7088932f12918c6ea9b01c031912d6f24e27
kenny30518 <A107070036@mail.shu.edu.tw>
2021-10-02 12:15:50 +0800

    add new page

---
+++ js/online.js
@@ -0,0 +1,10 @@
+//preloader//
+$(window).on('load', function(){
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector(".loading").style.visibility = "visible";
+    } else {
+        document.querySelector(".loading").style.display = "none";
+        document.querySelector("body").style.visibility = "visible";
+    }
+});

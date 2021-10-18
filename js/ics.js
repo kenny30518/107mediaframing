0e30a1b2bbd72a383075c80ac147ae1dbcc3417f
Kenny Chen <A107070036@mail.shu.edu.tw>
2021-10-19 01:28:46 +0800

    change burger menu

--- js/ics.js
+++ js/ics.js
@@ -26,57 +26,42 @@ tl.to(".loading",0.5,{
 	ease: Expo.easeInOut
 });
 
-//burger animation//
+//burger animation
+function show(){
+    document.querySelector('.menu').classList.toggle('active');
+    document.querySelector('.data').classList.toggle('active');
+}
+
 var tl = new TimelineMax({paused: true});
 
 tl.to(".item1",0.8,{
-	y: 6,
-	rotation: 45,
-	ease: Expo.easeInOut
+    y: 6,
+    rotation: 45,
+    ease: Expo.easeInOut
 });
 
 tl.to(".item2",0.8,{
-	opacity: 0,
-	ease: Expo.easeInOut,
-	delay: -0.8
+    opacity: 0,
+    ease: Expo.easeInOut,
+    delay: -0.8
 });
 
 tl.to(".item3",0.8,{
-	y: -10,
-	rotation: -225,
-	ease: Expo.easeInOut,
-	delay: -0.8
+    y: -8,
+    rotation: -225,
+    ease: Expo.easeInOut,
+    delay: -0.8
 });
-
-tl.to(".menu", 0.5, {
-	left:"0%",
-	ease: Expo.easeInOut,
-	delay: -1
-});
-
-tl.staggerFrom(".menu ul li", 1,{
-	x: -200,
-	rotation: -10,
-	opacity: 0,
-	ease: Expo.easeInOut,
-	delay: -1
-},0.3);
 
 tl.reverse();
-$(document).on("click", ".rwdBurger", function() {
-		tl.reversed(!tl.reversed());
+$(document).on("click", ".burgerwrapper", function() {
+        tl.reversed(!tl.reversed());
 });
 
 //scroll out animation
 ScrollOut({
 	targets: '.content_image,.intro h2,.intro h3'
 });
-
-//scroll to top
-window.addEventListener('scroll', function(){
-	var scroll = document.querySelector('.scrollToTop');
-	scroll.classList.toggle("active", window.scrollY > 500)
-})
 
 
 

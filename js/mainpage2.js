450decf143a81104e8245f7f85063089ddbcf6c4
Kenny Chen <A107070036@mail.shu.edu.tw>
2021-10-21 21:58:56 +0800

    add slider RWD layout

--- js/mainpage2.js
+++ js/mainpage2.js
@@ -184,7 +184,7 @@ $(document).ready(function(){
 	        arrows: false,
 	        centerMode: true,
 	        centerPadding: '40px',
-	        slidesToShow: 3
+	        slidesToShow: 2
 	      }
 	    },
 	    {

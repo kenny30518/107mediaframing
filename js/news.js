e58c959bce5da10dc9ec94b75a97dd446c7017b9
Kenny Chen <A107070036@mail.shu.edu.tw>
2021-10-21 18:11:25 +0800

    add content into ics.html

--- js/news.js
+++ js/news.js
@@ -15,6 +15,10 @@ function show(){
     document.querySelector('.data').classList.toggle('active');
 }
 
+function expend1(){
+    document.querySelector('.card p').innerHTML = "近幾年越來越多人關心糧食議題，其中也包含我們常聽見、看見的「小農」。什麼是小農？對於大學生來說，我們關注的是小農哪個面向？我們從大學生愛用的社群 Dcard 中發現，大學生們會在意自己所吃或購買的食物之品質。近年許多大學生前往咖啡廳或是自行購買鮮乳時，會特別選擇小農經典類型的飲品，或有特別標示「小農」、「品質把關」字樣的商品。就像小農鮮乳拿鐵對於大學生的吸引力勝過一般的鮮乳拿鐵！讓我們最新風向帶著大家進入 Dcard 一探大學生與小農之間的關聯吧！";
+}
+
 var tl = new TimelineMax({paused: true});
 
 tl.to(".item1",0.8,{

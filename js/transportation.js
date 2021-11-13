7fa910cb5085dee8f58cfcd1fe9e3d9cb64f116b
kenny30518 <A107070036@mail.shu.edu.tw>
2021-11-14 01:09:14 +0800

    add new content and fix footer top vertical align issue

--- js/transportation.js
+++ js/transportation.js
@@ -35,3 +35,20 @@ tl.reverse();
 $(document).on("click", ".burgerwrapper", function() {
         tl.reversed(!tl.reversed());
 });
+
+//change content by screen width
+$(window).on('load', function(){
+    if($( window ).width() <= 1066){
+        document.getElementById("railContent").innerHTML = "<p>觀眾自捷運淡水信義線（紅線）至中山站下車，於 2 號出口出站後，步行至，往前行走約 200 公尺，右手邊出現世新資傳畢展看板，即抵達PPP時尚藝文空間。</p>";
+    }else {
+        document.getElementById("railContent").innerHTML = "<p>觀眾自捷運淡水信義線（紅線）至中山站下車，於 2 號出口出站後，<br/>步行至，往前行走約 200 公尺，右手邊出現世新資傳畢展看板，<br/>即抵達PPP時尚藝文空間。</p>";
+    }
+});
+
+$( window ).resize(function() { 
+    if($( window ).width() <= 1066){
+        document.getElementById("railContent").innerHTML = "<p>觀眾自捷運淡水信義線（紅線）至中山站下車，於 2 號出口出站後，步行至，往前行走約 200 公尺，右手邊出現世新資傳畢展看板，即抵達PPP時尚藝文空間。</p>";
+    }else {
+        document.getElementById("railContent").innerHTML = "<p>觀眾自捷運淡水信義線（紅線）至中山站下車，於 2 號出口出站後，<br/>步行至，往前行走約 200 公尺，右手邊出現世新資傳畢展看板，<br/>即抵達PPP時尚藝文空間。</p>";
+    }
+});

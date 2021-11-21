f5123664fb75c596293047d1b52ea977bcf35448
Kenny Chen <A107070036@mail.shu.edu.tw>
2021-11-22 02:34:40 +0800

    change transportation links

---
+++ js/map.js
@@ -0,0 +1,89 @@
+//preloader//
+$(window).on('load', function(){
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector(".loading").style.visibility = "visible";
+    } else {
+        document.querySelector(".loading").style.display = "none";
+        document.querySelector("body").style.visibility = "visible";
+    }
+});
+
+//burger animation
+var tl = new TimelineMax({paused: true});
+
+tl.to(".item1",0.8,{
+    y: 6,
+    rotation: 45,
+    ease: Expo.easeInOut
+});
+
+tl.to(".item2",0.8,{
+    opacity: 0,
+    ease: Expo.easeInOut,
+    delay: -0.8
+});
+
+tl.to(".item3",0.8,{
+    y: -8,
+    rotation: -225,
+    ease: Expo.easeInOut,
+    delay: -0.8
+});
+
+tl.reverse();
+$(document).on("click", ".burgerwrapper", function() {
+    tl.reversed(!tl.reversed());
+});
+
+
+//Smooth scroll effect
+$('.trafficNav ul li a').on('click', function(e){
+    if(this.hash !== ''){
+        e.preventDefault();
+
+        const hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        },800);
+    }
+});
+
+/*
+//scroll to top animation
+$('.scrollToTop a').on('click', function(e){
+    if(this.hash !== ''){
+        e.preventDefault();
+
+        const hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        },800);
+    }
+});
+
+//show and hide scroll to top icon
+window.addEventListener('scroll', function(){
+    var scroll = document.querySelector('.scrollToTop');
+    scroll.classList.toggle("active", window.scrollY > 500)
+})
+*/
+
+//burger animation
+function show(){
+    document.querySelector('.menu').classList.toggle('active');
+    document.querySelector('.data').classList.toggle('active');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+

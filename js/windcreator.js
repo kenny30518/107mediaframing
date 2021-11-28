d25d941f0701c208ff09239ac99c2b7d5cdb2f1c
Kenny Chen <A107070036@mail.shu.edu.tw>
2021-11-29 02:59:14 +0800

    add new page

---
+++ js/windcreator.js
@@ -0,0 +1,72 @@
+//preloader//
+$(window).on('load', function(){
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector(".loading").style.visibility = "visible";
+    } else {
+        document.querySelector(".loading").style.display = "none";
+        document.querySelector("body").style.visibility = "visible";
+    }
+});
+
+$(window).on('load', function(){
+	$('html, body').animate({
+        scrollTop: $('start').offset().top
+ 	},800);
+});
+
+$('.next').on('click', function(e){
+	if(this.hash !== ''){
+        e.preventDefault();
+
+        const hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        },800);
+    }
+});
+
+var tl = new TimelineMax({onComplete:scrollDetect})
+
+tl.fromTo(".start h1",1,{
+	autoAlpha: 0
+},{
+	autoAlpha: 1
+},"+=0.5");
+
+tl.to(".start h1",1,{
+	autoAlpha: 0
+},"+=0.5");
+
+tl.fromTo(".introFirst",1,{
+	autoAlpha: 0
+},{
+	autoAlpha: 1
+},"+=0.5");
+
+tl.fromTo(".introSecond",1,{
+	autoAlpha: 0
+},{
+	autoAlpha: 1
+},"+=0.5");
+
+function scrollDetect(){
+	$(window).on('wheel', function() {
+        $('html, body').animate({
+            scrollTop: $('.s2').offset().top
+        },800);
+		return false;
+	});
+}
+
+
+
+
+
+
+
+
+
+
+
+
